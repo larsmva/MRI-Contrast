1d20e464b2ba31d51db2578aa1789c76009e4e95
larsmva <larsmvalnes@gmail.com>
2017-09-25 06:56:16 +0200

    update

--- mri-contrast.py
+++ mri-contrast.py
@@ -1,7 +1,24 @@
 from dolfin import *
 #from cbcpost import *
 #from cbcpost.utils import create_submesh, create_slice
+import numpy as np
 
+
+TR    = 5.12*10**-3
+TE    = 2.29*10**-3
+theta = 8.0
+r1,r2 = 3.6 , 6.3
+R1 = 0.92
+k1 = TR*r1
+k2  = TE*r2 
+aux0 = np.cos(np.deg2rad(theta)) 
+aux1= 1.-aux0
+aux2 = 1.+aux0  
+G =  np.exp(-TR*R1) 
+
+def BPII (c ):
+    return  ( (1.-aux0*G)*(1.-G*np.exp(-k1*c*10**-3))*np.exp(-k2*c*10**-3)/((1.-aux0*G*np.exp(-k1*c*10**-3))*(1.-G)) -1 )*100
+   
 
 def boundary(x, on_boundary): 
   return on_boundary
@@ -43,86 +60,89 @@ if __name__=='__main__':
 
 	U0 = Function(V)  # initial U
        
+
+        bpii = Function(V)
         
 ##################################################   
 
         dt_ = float(Z.dt)
-
-	D = Constant(float(Z.D))
+        print dt_
+	
         C = Constant(float(Z.C))
 	dt = Constant(dt_)
         Tend = Z.Tend
     
-        print Z.D, Tend   
-
+      
 
         if Z.init.endswith("h5"):
           in0= HDF5File(mesh.mpi_comm(),Z.init,"r") # basename
 	  in0.read(U0,"/mric")
           in0.close()
-          C = U0.vector().array().max() #5000 # parallel
+          C = Constant ( U0.vector().array().max() ) #5000 # parallel
           bc  = DirichletBC(V, C, boundary ) 
-          print C
+         
         else :
+          C = Constant(float(Z.C))
           bc  = DirichletBC(V, C, boundary ) 
           U0.vector()[:]=0
 
+        U_.assign(U0)
 #############################################################      
 	m =u*v*dx 
-	k = dt*D*inner(grad(v),grad(u))*dx
+	k = dt*Constant(Z.D)*dot(grad(v),grad(u))*dx
 
         L =U_*v*dx  
 
-
-
+       
+        print dt
 ###########################################################
-        action_form = action(m, Constant(1))
-	#A = assemble(a)
-        M= assemble(m)
+        dia =  assemble (action(m, Constant(1)))
+	M= assemble(m)
         M.zero()
-        M.set_diagonal(assemble(action_form))
+        M.set_diagonal(dia)
           
         K = assemble(k)
         A = M+K
-        U_.assign(U0)
-  
-	
+      
+        a = u*v*dx + dt*Constant(Z.D)*dot(grad(v),grad(u))*dx
+	A =assemble(a)
    
-	
+	bc.apply(A)
 
-   
 
-          
 	t = 0.0
 	time_step =0
         
-        save_resolution=0.1      
-	#writer = XDMFFile(mesh.mpi_comm(), Z.out)
-        outfile = File("resultsD%sdt%s/mri-contrast.pvd"%(Z.D,Z.dt)) 
+        save_resolution=0.01      
+
+        outfile = File("results%sD%sdt%s/mri-contrast.pvd"%(Z.out,Z.D,Z.dt)) 
         
         save_step = save_resolution*(Tend/dt_)
-        print save_step 
+
      
         print "solving"
 #################################################################
-	while t < Tend-dt_/2:
+	while t < Tend+dt_/2:
 
 
 	  t += dt_
 
           b = assemble(L)
           bc.apply(b)
-          bc.apply(A)
+          
 
-	  solve(A, U.vector(), b, "gmres" , "amg" )
+	  solve(A, U.vector(), b)
 
-          print assemble((U-U_)**2*dx)
           U_.assign(U)
-          U.vector()[:]
+
 ################################################################
-       
-	  if time_step%save_step==0:
-               outfile << U 
+         
+	  if time_step%save_step==0: 
+               bpii.vector()[:] = BPII(U.vector().array())
+               outfile << U
+               print time_step
+               
           time_step+=1
 	 
+        
 
